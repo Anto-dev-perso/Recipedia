ad545bbd6a121992f377797290192447bb31a41d
AntoC <antonin.coupanec@gmail.com>
2025-08-24 21:18:53 +0200

    test: add unit test for RecipeNumber component

--- tests/mocks/components/atomic/RoundButton-mock.tsx
+++ tests/mocks/components/atomic/RoundButton-mock.tsx
@@ -1,20 +1,19 @@
-import {Button, Text, View} from "react-native";
-import React from "react";
-import {RoundButtonProps} from "@components/atomic/RoundButton";
-
+import { Button, Text, View } from 'react-native';
+import React from 'react';
+import { RoundButtonProps } from '@components/atomic/RoundButton';
 
 export function roundButtonMock(roundButtonProps: RoundButtonProps) {
-
-    return (
-        <View>
-            <Text testID={roundButtonProps.testID + "::RoundButton::Icon"}>
-                {roundButtonProps.icon}
-            </Text>
-            {roundButtonProps.onPressFunction !== undefined ?
-                <Button testID={roundButtonProps.testID + "::RoundButton::OnPressFunction"}
-                        onPress={roundButtonProps.onPressFunction}
-                        title="Click on Text"/> : null}
-
-        </View>
-    );
+  return (
+    <View>
+      <Text testID={roundButtonProps.testID + '::RoundButton::Icon'}>{roundButtonProps.icon}</Text>
+      <Text testID={roundButtonProps.testID + '::RoundButton::Size'}>{roundButtonProps.size}</Text>
+      {roundButtonProps.onPressFunction !== undefined ? (
+        <Button
+          testID={roundButtonProps.testID + '::RoundButton::OnPressFunction'}
+          onPress={roundButtonProps.onPressFunction}
+          title='Click on Text'
+        />
+      ) : null}
+    </View>
+  );
 }
