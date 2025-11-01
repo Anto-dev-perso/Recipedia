7b3395df4cb7d886b96d60ce99808f32dab9742a
AntoC <antonin.coupanec@gmail.com>
2025-11-01 20:07:25 +0100

    feat(Recipe): add missing preparation time validation and translations
    - Included validation for missing preparation time in `Recipe.tsx`.
    - Updated French and English translations to support preparation time.

--- src/translations/en/alerts.ts
+++ src/translations/en/alerts.ts
@@ -5,7 +5,7 @@ export default {
     titlePlural: 'Missing elements',
     messageAll:
       "You haven't add any of the elements to your recipe. Please enter before validate at least: \
-                  \n\t- an image\n\t- a title\n\t- some ingredients\n\t- for how many persons this recipe is\n\t- some instructions for the preparation",
+                              \n\t- an image\n\t- a title\n\t- some ingredients\n\t- for how many persons this recipe is\n\t- some instructions for the preparation",
     messageSingularBeginning: "You're missing ",
     messageSingularEnding: ' to your recipe. Please add this before validate.',
     messageSingularNutrition:
@@ -18,6 +18,7 @@ export default {
     titleIngredients: 'some ingredients',
     titlePreparation: 'some instructions for the preparation',
     titlePersons: 'for how many persons this recipe is',
+    titleTime: 'a preparation time',
     ingredientQuantities: 'quantities for all ingredients',
     ingredientNames: 'names for all ingredients',
     nutrition: 'complete nutrition information',
