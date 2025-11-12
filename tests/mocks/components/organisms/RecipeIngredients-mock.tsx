1baf94fd43608e24a0d6830bff2140619effcca5
AntoC <antonin.coupanec@gmail.com>
2025-11-12 21:14:52 +0100

    test(Recipe): fix tests now that ingredients has changed
    Add a mock for `RecipeIngredients`.
    
    Fix all the tests in Recipe.test.tsx`

---
+++ tests/mocks/components/organisms/RecipeIngredients-mock.tsx
@@ -0,0 +1,66 @@
+import { Button, Text, View } from 'react-native';
+import React from 'react';
+import { RecipeIngredientsProps } from '@components/organisms/RecipeIngredients';
+
+export function recipeIngredientsMock(props: RecipeIngredientsProps) {
+  const { testID, ingredients, mode } = props;
+
+  return (
+    <View testID={testID}>
+      <Text testID={`${testID}::Mode`}>{mode}</Text>
+      <Text testID={`${testID}::Ingredients`}>{JSON.stringify(ingredients)}</Text>
+      {'prefixText' in props && <Text testID={`${testID}::PrefixText`}>{props.prefixText}</Text>}
+      {'columnTitles' in props && props.columnTitles && (
+        <View>
+          <Text testID={`${testID}::ColumnTitle1`}>{props.columnTitles.column1}</Text>
+          <Text testID={`${testID}::ColumnTitle2`}>{props.columnTitles.column2}</Text>
+          <Text testID={`${testID}::ColumnTitle3`}>{props.columnTitles.column3}</Text>
+        </View>
+      )}
+      {ingredients.map((ingredient, index) => (
+        <View key={index} testID={`${testID}::${index}::Row`}>
+          {mode === 'readOnly' ? (
+            <>
+              <Text testID={`${testID}::${index}::QuantityAndUnit`}>
+                {`${ingredient.quantity} ${ingredient.unit}`}
+              </Text>
+              <Text testID={`${testID}::${index}::IngredientName`}>{ingredient.name}</Text>
+            </>
+          ) : (
+            <>
+              <Text testID={`${testID}::${index}::QuantityInput`}>{ingredient.quantity}</Text>
+              <Text testID={`${testID}::${index}::UnitInput::CustomTextInput`}>
+                {ingredient.unit}
+              </Text>
+              <Text testID={`${testID}::${index}::NameInput::TextInputWithDropdown::Value`}>
+                {ingredient.name}
+              </Text>
+            </>
+          )}
+        </View>
+      ))}
+      {'openModal' in props && ingredients.length === 0 && (
+        <>
+          <Button
+            testID={`${testID}::OpenModal::RoundButton::OnPressFunction`}
+            onPress={props.openModal}
+            title='Open Modal'
+          />
+          <Text testID={`${testID}::OpenModal::RoundButton::Icon`}>line-scan</Text>
+        </>
+      )}
+      {'onAddIngredient' in props && (
+        <>
+          <Button
+            testID={`${testID}::AddButton::RoundButton::OnPressFunction`}
+            onPress={props.onAddIngredient}
+            title='Add Ingredient'
+          />
+          <Text testID={`${testID}::AddButton::RoundButton::Icon`}>
+            {mode === 'add' && ingredients.length === 0 ? 'pencil' : 'plus'}
+          </Text>
+        </>
+      )}
+    </View>
+  );
+}
