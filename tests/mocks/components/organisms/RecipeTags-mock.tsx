a39850e424e723e3e1c446995bea80490920b6de
AntoC <antonin.coupanec@gmail.com>
2025-11-17 22:31:20 +0100

    test(Recipe): update existing unit tests
    Add new tests in `Recipe` for those new cases.
    
    Update the mocks accordingly and create one for the validation queue

--- tests/mocks/components/organisms/RecipeTags-mock.tsx
+++ tests/mocks/components/organisms/RecipeTags-mock.tsx
@@ -1,28 +1,27 @@
-import React from "react";
-import {Button, Text, View} from "react-native";
-import {RecipeTagProps} from "@components/organisms/RecipeTags";
+import React from 'react';
+import { Button, Text, View } from 'react-native';
+import { RecipeTagProps } from '@components/organisms/RecipeTags';
 
 export function recipeTagsMock(recipeTagProp: RecipeTagProps) {
-
-    const tagsTestID = "RecipeTags";
-    return (
+  const tagsTestID = 'RecipeTags';
+  return (
+    <View>
+      <Text testID={tagsTestID + '::TagsList'}>{JSON.stringify(recipeTagProp.tagsList)}</Text>
+      {recipeTagProp.type === 'readOnly' ? null : (
         <View>
-            <Text testID={tagsTestID + "::TagsList"}>
-                {JSON.stringify(recipeTagProp.tagsList)}
-            </Text>
-            {recipeTagProp.type === 'readOnly' ? null :
-                <View>
-                    <Text testID={tagsTestID + "::RandomTags"}>
-                        {recipeTagProp.randomTags}
-                    </Text>
-                    <Button testID={tagsTestID + "::AddNewTag"}
-                            onPress={() => recipeTagProp.addNewTag(recipeTagProp.tagsList[0])}
-                            title="Add New Tag"/>
-                    <Button testID={tagsTestID + "::RemoveTag"}
-                            onPress={() => recipeTagProp.removeTag(recipeTagProp.tagsList[0])}
-                            title="Change Tag"/>
-                </View>
-            }
+          <Text testID={tagsTestID + '::RandomTags'}>{recipeTagProp.randomTags}</Text>
+          <Button
+            testID={tagsTestID + '::AddNewTag'}
+            onPress={() => recipeTagProp.addNewTag('mockTag')}
+            title='Add New Tag'
+          />
+          <Button
+            testID={tagsTestID + '::RemoveTag'}
+            onPress={() => recipeTagProp.removeTag(recipeTagProp.tagsList[0])}
+            title='Change Tag'
+          />
         </View>
-    );
+      )}
+    </View>
+  );
 }
